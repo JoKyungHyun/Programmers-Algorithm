428dc6999b210baa7ce3ff16753e9fd24d5a3335
JoKyungHyun <ckg3597@gmail.com>
2020-09-04 18:15:33 +0900

    Algoritm

---
+++ src/Level01/Problem01.java
@@ -0,0 +1,21 @@
+package Level01;
+
+public class Problem01 {
+	public static long solution(int a, int b) {
+    long sum = 0;
+    if(a > b){
+        for(int i = 1; i < (a-b); i++){
+            int index = i + b; 
+        	sum = index + sum;
+        }
+        return sum + a + b;
+    }else if(a < b) {
+    	for(int i = 1; i < (b-a); i++){
+            int index = i + a; 
+        	sum = index + sum;
+        }
+        return sum + a + b;
+    }else
+    	return a;
+	}
+}
