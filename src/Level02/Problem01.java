ed5bf8e0689684cc6219f363492b854672ae43d3
JoKyungHyun <ckg3597@gmail.com>
2020-10-10 01:08:16 +0900

    no message

---
+++ src/Level02/Problem01.java
@@ -0,0 +1,23 @@
+package Level02;
+
+public class Problem01 {
+	public static int[] solution(int[] prices) {
+		int[] answer = {};
+
+		for (int i = 0; i < prices.length; i++) {
+			int sum = 0;
+			for (int j = i + 1; j < prices.length; j++) {
+
+				if (prices[i] < prices[j]) {
+					sum = sum + 1;
+				} else {
+					break;
+				}
+			}
+			prices[i] = sum;
+		}
+
+		return answer;
+	}
+
+}
